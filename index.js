e9e0e44d27929f14d183cbf6b0fc78d58f9e6914
Abed Nganga <ngangaabed@gmail.com>
2023-06-09 22:42:29 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger');
+    let left = parseInt(dodger.style.left) || 0;
+    left -= 1;
+    dodger.style.left = `${left}px`;
+  }
+  
+  function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    let left = parseInt(dodger.style.left) || 0;
+    left += 1;
+    dodger.style.left = `${left}px`;
+  }
